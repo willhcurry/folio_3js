63267dd84c79570b5d413f5310bff01889e4a43e
willhcurry <willhcurry@gmail.com>
2023-11-10 16:14:48 -0700

    connected world class

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import Camera from './Camera.js';
 import Renderer from './Renderer.js';
 import Loop from './Utils/Loop.js';
+import World from './World/World.js';
 
 let instance = null;
 
@@ -16,6 +17,8 @@ export default class App {
         this.scene = new THREE.Scene();
         this.camera = new Camera();
         this.renderer = new Renderer();
+        this.world = new World();
         this.loop = new Loop();
+        
     };
 };
