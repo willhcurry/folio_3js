1057846a75be1ac147d6d11e7feb7eed61d5dbbd
willhcurry <willhcurry@gmail.com>
2023-11-25 23:04:55 -0700

    More setup

--- src/App/World/Physics.js
+++ src/App/World/Physics.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import App from '../App.js';
-import { appStateStore } from '../Utils/Store.js';
+import * as THREE from "three";
+import App from "../App.js";
+import { appStateStore } from "../Utils/Store.js";
 
 /**
  * Class representing a physics simulation
@@ -10,12 +10,15 @@ export default class Physics {
    * Creates an instance of Physics.
    */
   constructor() {
+    // getting the app instance
     this.app = new App();
     this.scene = this.app.scene;
 
-    this.meshmap = new Map();
+    // setting the physics map
+    this.meshMap = new Map();
 
-    import('@dimforge/rapier3d').then((RAPIER) => {
+    // setting the physics world
+    import("@dimforge/rapier3d").then((RAPIER) => {
       const gravity = { x: 0, y: -9.81, z: 0 };
       this.world = new RAPIER.World(gravity);
       this.rapier = RAPIER;
@@ -34,23 +37,24 @@ export default class Physics {
   add(mesh, type, collider) {
     // defining the rigid body type
     let rigidBodyType;
-    switch (type) {
+    switch(type) {
       case "dynamic":
-        rigidBodyType = this.rapier.RigidBodyDesc.dynamic();
-        break;
-      case "fixed":
-        rigidBodyType = this.rapier.RigidBodyDesc.fixed();
-        break;
-      case "kinematic":
-        rigidBodyType = this.rapier.RigidBodyDesc.kinematicPositionBased();
-        break;
+      rigidBodyType = this.rapier.RigidBodyDesc.dynamic();
+      break;
+    case "fixed":
+      rigidBodyType = this.rapier.RigidBodyDesc.fixed();
+      break
+    case 'kinematic':
+      rigidBodyType = this.rapier.RigidBodyDesc.kinematicPositionBased();
+      break;
     }
     this.rigidBody = this.world.createRigidBody(rigidBodyType);
 
+    // defining the collider type
     let colliderType;
 
     switch (collider) {
-      case 'cuboid':
+      case "cuboid":
         const dimensions = this.computeCuboidDimensions(mesh);
         colliderType = this.rapier.ColliderDesc.cuboid(
           dimensions.x / 2,
@@ -59,18 +63,19 @@ export default class Physics {
         );
         this.world.createCollider(colliderType, this.rigidBody);
         break;
-      case 'ball':
+      case "ball":
         const radius = this.computeBallDimensions(mesh);
         colliderType = this.rapier.ColliderDesc.ball(radius);
         this.world.createCollider(colliderType, this.rigidBody);
         break;
-      case 'trimesh':
+      case "trimesh":
         const { scaledVertices, indices } = this.computeTrimeshDimensions(mesh);
         colliderType = this.rapier.ColliderDesc.trimesh(
           scaledVertices,
           indices
         );
         this.world.createCollider(colliderType, this.rigidBody);
+
         break;
     }
 
@@ -80,8 +85,7 @@ export default class Physics {
     this.rigidBody.setTranslation(worldPosition);
     this.rigidBody.setRotation(worldRotation);
 
-    this.meshmap.set(mesh, this.rigidBody);
-
+    this.meshMap.set(mesh, this.rigidBody);
     return this.rigidBody;
   }
 
@@ -97,13 +101,15 @@ export default class Physics {
     size.multiply(worldScale);
     return size;
   }
-   
-  
+
   /**
-  Computes the radius of a sphere collider for a given mesh
-  @param {THREE.Mesh} mesh - The mesh to compute the radius for
-  @returns {number} The radius of the sphere collider
-  */
+   * Computes the radius of a
+   /**
+
+Computes the radius of a sphere collider for a given mesh
+@param {THREE.Mesh} mesh - The mesh to compute the radius for
+@returns {number} The radius of the sphere collider
+*/
   computeBallDimensions(mesh) {
     mesh.geometry.computeBoundingSphere();
     const radius = mesh.geometry.boundingSphere.radius;
@@ -121,29 +127,38 @@ export default class Physics {
     const vertices = mesh.geometry.attributes.position.array;
     const indices = mesh.geometry.index.array;
     const worldScale = mesh.getWorldScale(new THREE.Vector3());
-    console.log(worldScale);
-
     const scaledVertices = vertices.map((vertex, index) => {
       return vertex * worldScale.getComponent(index % 3);
     });
-
-    return { scaledVertices, vertices, indices };
+    return { scaledVertices, indices };
   }
-
   /**
+  
   The loop function that updates the physics simulation and the mesh positions and rotations
   */
   loop() {
     if (!this.rapierLoaded) return;
-
     this.world.step();
+    this.meshMap.forEach((rigidBody, mesh) => {
 
-    this.meshmap.forEach((rigidBody, mesh) => {
+      // extracting the position and rotation from the rigid body
       const position = new THREE.Vector3().copy(rigidBody.translation());
-      const rotation = rigidBody.rotation();
+      const rotation = new THREE.Quaternion().copy(rigidBody.rotation());
 
-      mesh.parent.worldToLocal(position);
+      // transforming the position to the parent mesh's local space
+      position.applyMatrix4(
+        new THREE.Matrix4().copy(mesh.parent.matrixWorld).invert()
+      );
 
+      // transforming the rotation to the parent mesh's local space
+      const inverseParentMatrix = new THREE.Matrix4()
+        .extractRotation(mesh.parent.matrixWorld)
+        .invert();
+      const inverseParentRotation =
+        new THREE.Quaternion().setFromRotationMatrix(inverseParentMatrix);
+      rotation.premultiply(inverseParentRotation);
+
+      
       mesh.position.copy(position);
       mesh.quaternion.copy(rotation);
     });
