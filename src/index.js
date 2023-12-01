da6f0a7f477ee4b8f2c59a707bcdc80da0b6e124
willhcurry <willhcurry@gmail.com>
2023-12-01 16:41:04 -0700

    testing gh-pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './style.css';
-import App from './App/App.js';
+import 'style.css';
+import 'assets/fonts/helvetiker_regular.typeface.json';
+import 'assets/models/scene.gltf';
 
 const app = new App();
