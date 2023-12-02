a6e948a45a5197742897bb2cc8ffac491fd363bf
willhcurry <willhcurry@users.noreply.github.com>
2023-12-02 02:29:14 +0000

    deploy: cf20d09e7628e1ad8ae996dcd92cf0c498ab095b

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import './style.css'
-import App from './App/App.js'
-
-const app = new App()
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-
-export default {
-  root: 'src/',
-  publicDir: '../static/',
-  base: './',
-  plugins: [wasm(), topLevelAwait()],
-};
