abf99c6e23f773d74be44a22fac460c75df96de0
willhcurry <willhcurry@gmail.com>
2024-12-17 16:30:41 -0700

    feat: add modals and portal interactions with improved UX - Added permanent control instructions UI

--- src/App/UI/Preloader.js
+++ src/App/UI/Preloader.js
@@ -11,43 +11,37 @@ export default class Preloader {
         this.startButton = document.querySelector('.start')
 
         this.assetStore.subscribe((state) =>{
-
             this.numberOfLoadedAssets = Object.keys(state.loadedAssets).length
             this.numberOfAssetsToLoad = state.assetsToLoad.length
             this.progress = this.numberOfLoadedAssets / this.numberOfAssetsToLoad
             this.progress = Math.trunc(this.progress * 100)
-            document.getElementById('progressPercentage').innerHTML= this.progress
+            document.getElementById('progressPercentage').innerHTML = this.progress
 
-            if (this.progress === 100)
-            {   
+            if (this.progress === 100) {
                 appStateStore.setState({assetsReady: true})
                 this.loading.classList.add('fade')
                 window.setTimeout(() => this.ready(), 1200)
-                
             }
         })
-        
     }
 
     ready() {
-
         this.loading.remove()
 
         this.startButton.style.display = 'inline'
         this.startButton.classList.add('fadeIn')
 
         this.startButton.addEventListener('click', () => {
-            console.log('started')
             this.overlay.classList.add('fade')
             this.startButton.classList.add('fadeOut')
+            
+            // Show the permanent controls
+            document.querySelector('.permanent-controls').classList.add('show')
 
             window.setTimeout(() => {
                 this.overlay.remove()
                 this.startButton.remove()
             }, 2000)
-
-
-
-        },{once: true})
+        }, {once: true})
     }
 }
